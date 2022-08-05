8a27a8799bacb129f6a749c4c453f3d50910e5fc
Liam Jensen <jensen97@purdue.edu>
2022-08-05 17:51:31 -0400

    add basic math functions in js and 0 button for calculator

---
+++ app.js
@@ -0,0 +1,35 @@
+const operators = ['+', '-', '*', '/'];
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator){
+    if(operators.includes(operator)){
+        if(operator === '+'){
+            return add(num1,num2);
+        }
+        else if(operator === '-'){
+            return subtract(num1,num2);
+        }
+        else if(operator === '*'){
+            return multiply(num1, num2);
+        }
+        else if(operator === '/'){
+            return divide(num1, num2);
+        }
+    }
+    return 'Please use a math operation';
+}
